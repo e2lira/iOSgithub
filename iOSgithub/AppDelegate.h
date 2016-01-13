3f49387d875baf957625c4942267412a0d5d67e4
e2lira <e2lira@gmail.com>
2016-01-13 16:58:21 -0600

    Initial Commit

---
+++ iOSgithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSgithub
+//
+//  Created by Edgar Lira on 13/01/16.
+//  Copyright © 2016 e2lira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSgithub
+//
+//  Created by Edgar Lira on 13/01/16.
+//  Copyright © 2016 e2lira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
